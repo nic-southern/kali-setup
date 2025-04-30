93c61786b275e59f9f66975222def33cfc05a46c
Nic <1882319+nic-southern@users.noreply.github.com>
2025-04-30 07:04:59 -0500

    add more tools

--- setup.sh
+++ setup.sh
@@ -27,3 +27,21 @@ chmod +x /usr/share/reverse-shells/*
 # Extract rockyou.txt
 gzip -d /usr/share/wordlists/rockyou.txt.gz
 
+# Get Linpeas binary
+wget https://github.com/peass-ng/PEASS-ng/releases/latest/download/linpeas.sh -O /usr/share/linpeas.sh
+
+# Make linpeas executable
+chmod +x /usr/share/linpeas.sh
+
+# Get Linux Exploit Suggester 2
+wget -O /usr/share/les.sh https://raw.githubusercontent.com/mzet-/linux-exploit-suggester/master/linux-exploit-suggester.sh
+
+# Make les executable
+chmod +x /usr/share/les.sh
+
+# Get Powercat
+git clone https://github.com/besimorhino/powercat.git /usr/share/powercat
+
+# Make powercat executable
+chmod +x /usr/share/powercat/powercat.sh
+
